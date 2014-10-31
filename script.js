cea68ad4c8f48f9a91ca98875dec11344c38b03d
Richard Czechowski <richardczechowski@gmail.com>
2014-10-31 15:30:33 -0700

    Adds and completes index.html, survey.html w/o css. Adds and starts results.html

--- script.js
+++ script.js
@@ -1,22 +1,20 @@
 $(function() {
   console.log("Document Ready");
   Parse.initialize("It43BqxAW9BCoNjAKnGV9kKsQ4JRSZJfwTBxKMVn", "IT27sQWrzcw9V6M3IuEXrAA8gQ8a6yinpuwJDjdn");
-
-
-  function addBusiness(email, target, problem, solution, cost, successCb){
+  //Add a business idea to Parse
+  function addBusiness(email, surveyName, target, problem, solution, cost, successCb){
     var Business = Parse.Object.extend("Business");
     var business = new Business();
-    var parsePromise =  business.save({email: email, target: target, problem: problem, solution: solution, cost: cost })
-
+    var parsePromise =  business.save({email: email, surveyName: surveyName, target: target, problem: problem, solution: solution, cost: cost })
     parsePromise.then(successCb, function(error){
       alert("Fail Motha Fucka");
     })
-
   }
 
-  $("form").on("submit", function(){
-    debugger;
+  //business idea submit
+  $("#businessIdea").on("submit", function(){
     var email= $("input[name=email]").val();
+    var surveyName =$("input[name=surveyName]").val();
     var target= $("input[name=target]").val();
     var problem= $("input[name=problem]").val();
     var solution= $("input[name=solution]").val();
@@ -25,26 +23,33 @@ $(function() {
       resetForm($("form")[0]);
       console.log("Hip Hip Hurray Mutha Fucka");
     }
-    addBusiness(email, target, problem, solution, cost, onSuccess);
+    addBusiness(email, surveyName, target, problem, solution, cost, onSuccess);
     return false;
   })
 
+  //reset form on submit
   function resetForm(formElement){
     formElement.reset();
   }
 
-
-  var fillSurvey= function(){
+  //fill survey page with data from parse
+  var fillSurvey= function(surveyName){
     console.log("loading from Parse");
-
-    var surveyInfo = Parse.Object.extend("Business");
-    var query = new Parse.Query(business);
-    query.find({
-      success: function(results) {
-      var newLIs=results.map(function(element){
-      return $("<li>",{text: element.attributes.name + ":" + element.attributes.email});
-      });
-      $("ul#list").html(newLIs);
+    var Business= Parse.Object.extend("Business");
+    var query = new Parse.Query(Business);
+    var object=[];
+    query.equalTo("surveyName",surveyName);
+    query.first({
+      success: function(object) {
+        $("h1").replaceWith("<h1 class='text-center'>"+surveyName+"</h1>");
+        $("form").show();
+        $("#surveyName").hide();
+        var data=[object.attributes.target, object.attributes.problem, object.attributes.solution, object.attributes.cost]
+        var label=["target","problem","solution","cost"]
+        for (i=0; i<data.length;i++){
+          console.log(data[i].toString());
+          $("#"+label[i]+"Label > p span").text(data[i].toString());
+        }
       },
       error: function(error) {
         alert("Error: " + error.code + " " + error.message);
@@ -52,9 +57,13 @@ $(function() {
     });
   }
 
-  if ($("title").val()=="Business Survey"){
-   console.log("W3rks");
-    //fillSurvey();
-  }
+  $("#surveyName").on("submit", function(){
+    var surveyName= $("input[name=surveyName]").val();
+    if ($("title#survey").length){
+      console.log(surveyName);
+      fillSurvey(surveyName);
+    }
+      return false;
+  });
 
 });
